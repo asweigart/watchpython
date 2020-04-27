9b73d848782787c367414e85011136a2f9797e86
Al Sweigart <asweigart@gmail.com>
2020-04-26 22:22:58 -0700

    Basic working version of watchpython with an --interval option.

--- watchpython.py
+++ watchpython.py
@@ -3,15 +3,39 @@
 
 __version__ = '0.1.0'
 
+import os
+import subprocess
+import sys
+import time
+
 import click
 
 @click.command()
-@click.option("--count", default=1, help="Number of greetings.")
-@click.argument('nameeee')
-def main(count, nameeee):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for _ in range(count):
-        click.echo(f"Hello, {nameeee}!")
+@click.option('-n', '--interval', default=2, help='Seconds to wait between updates.')
+@click.argument('command')
+def main(command, interval):
+    """Run the given command."""
+    clearScreen()
+    commandStdOuput = ''
+    try:
+        while True:
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+
+            if result.stdout != commandStdOuput:
+                commandStdOuput = result.stdout
+                clearScreen()
+                click.echo(result.stdout)
+
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+def clearScreen():
+    if sys.platform == 'win32':
+        os.system('cls')  # Windows uses the cls command.
+    else:
+        os.system('clear')  # macOS and Linux use the clear command.
 
 if __name__ == '__main__':
     main()
